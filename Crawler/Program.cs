4c9dd11474b93fca3aac7717fa800f2dcad9d77f
Arkadiusz Zieliński <azielinski@digitalvirgo.com>
2022-10-15 11:02:56 +0200

    MAIN Improvement: Add main program to find emails on websites

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Crawler.models;
+
+HttpClient httpClient = new HttpClient();
+EmailSearcher emailSearcher = new EmailSearcher(httpClient);
+
+String url = args[0];
+HashSet<String> emailsFounded = await emailSearcher.Search(url);
+
+PrintFoundedEmails(emailsFounded);
+
+void PrintFoundedEmails(HashSet<String> emailsFounded)
+{
+    if (emailsFounded != null && emailsFounded.Any())
+    {
+        PrintEmails(emailsFounded);
+    }
+}
+
+void PrintEmails(HashSet<string> emails)
+{
+    foreach (string email in emails)
+    {
+        Console.WriteLine(email);
+    }
+}
+
