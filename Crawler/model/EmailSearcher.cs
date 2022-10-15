bc6d4041ed756155aaa4cb03603254c8c9be1680
Arkadiusz Zieliński <azielinski@digitalvirgo.com>
2022-10-15 22:22:37 +0200

    MAIN improvement: Refactor EmailSearcher.cs

--- Crawler/model/EmailSearcher.cs
+++ Crawler/model/EmailSearcher.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Crawler.models
 {
     public class EmailSearcher
     {
-
         private static readonly string USERNAME_REGEX = "[A-Z0-9._%+-]+";
         private static readonly string DOMAIN_REGEX = "[A-Z0-9.-]+\\.[A-Z]{2,}";
         private static readonly string EMAIL_REGEX = "\\b" + USERNAME_REGEX + "@" + DOMAIN_REGEX + "\\b";
-        private static readonly Regex EMAIL_PATTERN = new Regex(EMAIL_REGEX, RegexOptions.IgnoreCase);
+        private static readonly Regex EMAIL_PATTERN = new(EMAIL_REGEX, RegexOptions.IgnoreCase);
 
-        private HttpClient httpClient;
+        private readonly HttpClient httpClient;
 
         public EmailSearcher(HttpClient httpClient)
         {
             this.httpClient = httpClient;
+        }
+
+        private static HashSet<string> MatchEmails(string response)
+        {
+            MatchCollection emailMatches = EMAIL_PATTERN.Matches(response);
+            return emailMatches.Cast<Match>()
+                .Select(m => m.Value).ToHashSet();
         }
 
         public async Task<HashSet<string>> Search(string url)
@@ -34,13 +35,5 @@ namespace Crawler.models
             httpClient.Dispose();
             return await httpResponse.Content.ReadAsStringAsync();
         }
-
-        private HashSet<string> MatchEmails(string response)
-        {
-            MatchCollection emailMatches = EMAIL_PATTERN.Matches(response);
-            return emailMatches.Cast<Match>()
-                .Select(m => m.Value).ToHashSet();
-        }
-
     }
 }
